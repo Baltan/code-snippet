a5eee41c326eea7c1cd3f2caf0776b590591fb52
baltan <617640006@qq.com>
2022-09-07 14:16:21 +0800

    Optional.ofNullable() demo

---
+++ src/main/java/optional_test/Test3.java
@@ -0,0 +1,12 @@
+package optional_test;
+
+import java.util.Optional;
+
+public class Test3 {
+    public static void main(String[] args) {
+        // System.out.println(Optional.ofNullable(null).get());
+        System.out.println(Optional.ofNullable(null).orElse(0));
+        System.out.println(Optional.ofNullable(1).get());
+        System.out.println(Optional.ofNullable(1).orElse(0));
+    }
+}
