e23a978779204a6271c147c1aa7dabd6d292bda6
Baltan <617640006@qq.com>
2023-03-23 16:06:00 +0800

    动态写word

---
+++ src/main/java/word_test/RelativeInfo.java
@@ -0,0 +1,22 @@
+package word_test;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Description:
+ *
+ * @author Baltan
+ * @date 2020-11-01 15:04
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RelativeInfo {
+    private String relationship;
+    private String name;
+    private String relativeIdCard;
+    private String politicsStatus;
+    private String work;
+}
