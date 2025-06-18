466c17845f74299db9f3472d26bb05bc5734c30b
baltan <617640006@qq.com>
2025-06-18 17:27:42 +0800

    监听港股通股票当日行情

---
+++ src/main/java/news/HongKongStockIntradayConditionListener.java
@@ -0,0 +1,107 @@
+package news;
+
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import message_test.MessageUtil;
+
+import java.time.LocalTime;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+/**
+ * Description: 监听港股通股票当日行情
+ *
+ * @author baltan
+ * @date 2025/6/18 15:32
+ */
+public class HongKongStockIntradayConditionListener {
+    private static final String URL = "https://push2.eastmoney.com/api/qt/clist/get?np=1&fltt=1&invt=2&cb=jQuery37108670217273211989_1750231509621&fs=b:MK0144&fields=f12,f13,f14,f19,f1,f2,f4,f3,f152,f17,f18,f15,f16,f5,f6&fid=f3&pn=1&pz=5000&po=1&dect=1&ut=fa5fd1943c7b386f172d6893dbfba10b&wbp2u=6117356146666876|0|1|0|web&_=1750231509634";
+    private static final LocalTime END_TIME = LocalTime.of(16, 10, 0);
+    private static final Integer CONSECUTIVE_RISING_STOCK_DEQUE_SIZE = 5;
+    private static final ConcurrentHashMap<String, Deque<HongKongStockIntradayConditionResponse.Data.Stock>> CONSECUTIVE_RISING_STOCK_MAP = new ConcurrentHashMap<>();
+
+    public static void main(String[] args) {
+        while (true) {
+            if (LocalTime.now().isAfter(END_TIME)) {
+                break;
+            }
+            startListening();
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void startListening() {
+        try (HttpResponse httpResponse = HttpUtil.createGet(URL).execute()) {
+            if (httpResponse.isOk()) {
+                int start = httpResponse.body().indexOf("(") + 1;
+                int end = httpResponse.body().lastIndexOf(")");
+                HongKongStockIntradayConditionResponse response = JSON.parseObject(httpResponse.body().substring(start, end), HongKongStockIntradayConditionResponse.class);
+                List<HongKongStockIntradayConditionResponse.Data.Stock> stocks = response.getData().getDiff();
+                consecutiveRisingNotice(stocks);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 连续{@link CONSECUTIVE_RISING_STOCK_DEQUE_SIZE}次请求得到实时股价非递减，且有上涨趋势
+     *
+     * @param stocks
+     */
+    private static void consecutiveRisingNotice(List<HongKongStockIntradayConditionResponse.Data.Stock> stocks) {
+        stocks.parallelStream()
+                .forEach(stock -> {
+                    Deque<HongKongStockIntradayConditionResponse.Data.Stock> stockDeque =
+                            CONSECUTIVE_RISING_STOCK_MAP.computeIfAbsent(stock.getF12(), i -> new ArrayDeque<>(CONSECUTIVE_RISING_STOCK_DEQUE_SIZE));
+                    stockDeque.offerLast(stock);
+                    while (stockDeque.size() > CONSECUTIVE_RISING_STOCK_DEQUE_SIZE) {
+                        stockDeque.pollFirst();
+                    }
+                    if (stockDeque.size() < CONSECUTIVE_RISING_STOCK_DEQUE_SIZE) {
+                        return;
+                    }
+                    List<Double> prices = stockDeque.stream()
+                            .map(x -> ((double) x.getF2()) / 100)
+                            .collect(Collectors.toList());
+                    boolean risingFlag = false;
+                    for (int i = 1; i < prices.size(); i++) {
+                        if (prices.get(i - 1) > prices.get(i)) {
+                            return;
+                        } else if (prices.get(i - 1) < prices.get(i)) {
+                            risingFlag = true;
+                        }
+                    }
+                    if (risingFlag) {
+                        String content = String.format("%s（%s）股价持续拉升，近%d次获取到股价为%s元，实时成交额为%s", stock.getF14(), stock.getF12(), CONSECUTIVE_RISING_STOCK_DEQUE_SIZE, prices, getAmount(stock.getF6(), Boolean.FALSE));
+                        MessageUtil.sendMessage(content, Boolean.TRUE);
+                    }
+                });
+    }
+
+    /**
+     * 获取金额
+     *
+     * @param value    接口响应金额
+     * @param centUnit 接口响应金额单位是否为【分】
+     * @return
+     */
+    private static String getAmount(double value, boolean centUnit) {
+        if (value >= 100_000_000) {
+            return value / 100_000_000 / (centUnit ? 100 : 1) + "亿元";
+        } else if (value >= 10_000) {
+            return value / 10_000 / (centUnit ? 100 : 1) + "万元";
+        } else {
+            return value / (centUnit ? 100 : 1) + "元";
+        }
+    }
+}
---
+++ src/main/java/news/HongKongStockIntradayConditionResponse.java
@@ -0,0 +1,81 @@
+package news;
+
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * Description:
+ *
+ * @author baltan
+ * @date 2025/5/12 17:08
+ */
+@lombok.Data
+@Accessors(chain = true)
+public class HongKongStockIntradayConditionResponse {
+    private Integer rc;
+    private Integer rt;
+    private Long svr;
+    private Integer lt;
+    private Integer full;
+    private String dlmkts;
+    private Data data;
+
+    @lombok.Data
+    public static class Data {
+        private Integer total;
+        private List<Stock> diff;
+
+        @lombok.Data
+        public static class Stock {
+            private Integer f1;
+            /**
+             * 最新价（港币（分））
+             */
+            private Integer f2;
+            /**
+             * 涨跌幅（%）
+             */
+            private Integer f3;
+            /**
+             * 涨跌额（港币（分））
+             */
+            private Integer f4;
+            /**
+             * 成交量（股）
+             */
+            private Long f5;
+            /**
+             * 成交额（港币（元））
+             */
+            private Double f6;
+            /**
+             * 股票代码
+             */
+            private String f12;
+            private Integer f13;
+            /**
+             * 股票名称
+             */
+            private String f14;
+            /**
+             * 最高价（港币（分））
+             */
+            private Integer f15;
+            /**
+             * 最低价（港币（分））
+             */
+            private Integer f16;
+            /**
+             * 今日开盘价（港币（分））
+             */
+            private Integer f17;
+            /**
+             * 昨日收盘价（港币（分））
+             */
+            private Integer f18;
+            private Integer f19;
+            private Integer f152;
+        }
+    }
+}
